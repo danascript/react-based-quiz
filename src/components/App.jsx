35903d749866b38dc77345cc7328ee024da1fa0c
Daniela <daniela.fedyakin@gmail.com>
2018-05-07 10:07:51 +0200

    Files setup

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="display-4">This is the App Component!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
